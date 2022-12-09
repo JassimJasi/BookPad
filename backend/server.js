2396f944292c8d6431b250463ad8ca5f3f158c25
JassimJasi <jassimsadanamks@gmail.com>
2022-12-09 21:33:38 +0530

    socket io bug fixed

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,56 @@ mongoose
   .then(() => console.log("Database connected successfully"))
   .catch((err) => console.log("Error connection to database ", err));
 
+//<<<<<<<<<<<<<socket io>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  // if its the same user we are not going to add
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when connect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user?.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+//<<<<<<<<<<<<<socket io>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
